0f65bfa27c25caad7d89f4d4d2353892a984ad4b
bassilalcheikh <bassil.alcheikh@gmail.com>
2019-04-12 00:58:00 -0500

    Add files via upload
    Rough script for creating table. Still need to add a lot, namely to check if table exists. Need to make environment-agnostic.

---
+++ createSearchListTable.sql
@@ -0,0 +1,21 @@
+
+/*
+INSERT INTO searchlist VALUES
+(1, 'Rolex 16750', 'price-low', null, ' 2001', null, 'watch', 0 ),
+(2, 'Rolex 16760', 'price-low', null, '2001', null, 'watch', 0 ),
+(3, 'Rolex 126710BLRO', 'price-low', null, '5001', null, 'watch', 0 ),
+(4, 'Rolex 16610', 'price-low', null, '2001', null, 'watch', 0 ),
+(5, 'Omega Moonwatch 311.30.42.30.01.005', 'price-low', null, '2001', null, 'watch', 0 );
+*/
+/*
+CREATE TABLE searchlist(
+    search_id int NOT NULL PRIMARY KEY,
+    search_keyword varchar(255) NOT NULL,
+    search_criteria_1 varchar(50),
+    search_criteria_2 varchar(50),
+    search_values_1 varchar(50),
+    search_values_2 varchar(50),
+    item_type varchar(100),
+    item_quantity int
+);
+*/
