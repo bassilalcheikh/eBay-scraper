616ec275896ac2df19123581b8b714fe3663ca0d
bassilalcheikh <bassil.alcheikh@gmail.com>
2019-04-25 11:28:07 -0500

    Update createSearchListTable.sql

--- createSearchListTable.sql
+++ createSearchListTable.sql
@@ -15,5 +15,5 @@ INSERT INTO searchlist VALUES
 (3, 'Rolex', '126710BLRO',                     5001, null, 'watch', 0 ),
 (4, 'Rolex', '16610',                          2001, null, 'watch', 0 ),
 (5, 'Omega', 'Moonwatch 311.30.42.30.01.005',  2001, null, 'watch', 0 );
-(6, "Tudor', '79830",                          1001, NULL, "watch", 0 );
+(6, 'Tudor', '79830',                          1001, null, 'watch', 0 );
 */
