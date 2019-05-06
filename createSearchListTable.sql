d9e44c83825aa951ddb14222ac1d6944f0d81d8c
bassilalcheikh <bassil.alcheikh@gmail.com>
2019-05-05 20:43:54 -0500

    Update createSearchListTable.sql

--- createSearchListTable.sql
+++ createSearchListTable.sql
@@ -1,17 +1,19 @@
-/*
-INSERT INTO searchlist VALUES
-(1, 'Rolex 16750',      3001, null, 'watch', 0 ),
-(2, 'Rolex 16760',      3001, null, 'watch', 0 ),
-(3, 'Rolex 126710BLRO', 6001, null, 'watch', 0 ),
-(4, 'Rolex 16610',      3001, null, 'watch', 0 ),
-(5, 'Omega Moonwatch 311.30.42.30.01.005', 2001, null, 'watch', 0 );
-*/
-
-CREATE TABLE searchlist(
+CREATE TABLE IF NOT EXISTS searchlist(
     search_id int NOT NULL PRIMARY KEY,
-    search_keywords varchar(255) NOT NULL,
+    search_brand varchar(100) NOT NULL,
+    search_model varchar(100) NOT NULL,
     price_min real,
     price_max real,
     item_type varchar(100),
     item_quantity int
 );
+
+/*
+INSERT INTO searchlist VALUES
+(1, 'Rolex', '16750',      3001, null, 'watch', 0 ),
+(2, 'Rolex', '16760',      3001, null, 'watch', 0 ),
+(3, 'Rolex', '126710BLRO', 6001, null, 'watch', 0 ),
+(4, 'Rolex', '16610',      3001, null, 'watch', 0 ),
+(5, 'Omega', '311.30.42.30.01.005', 2001, null, 'watch', 0 )
+(6, 'Tudor', '79830RB', 1000, null, 'watch', 0);
+*/
