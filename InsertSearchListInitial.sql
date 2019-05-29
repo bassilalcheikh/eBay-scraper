235d49c719f5e8cab019e84bd5870a2bd301e5c0
bassilalcheikh <bassil.alcheikh@gmail.com>
2019-05-29 15:54:25 -0500

    Updated minimum price values
    Maybe in the future, a machine learning algorithm can address this?

--- InsertSearchListInitial.sql
+++ InsertSearchListInitial.sql
@@ -1,8 +1,16 @@
-
 INSERT OR IGNORE INTO searchlist VALUES
-(1, 'Rolex', '16750',      3001, null, 'watch', 0 ),
-(2, 'Rolex', '16760',      3001, null, 'watch', 0 ),
-(3, 'Rolex', '126710BLRO', 6001, null, 'watch', 0 ),
-(4, 'Rolex', '16610',      3001, null, 'watch', 0 ),
-(5, 'Omega', '311.30.42.30.01.005', 2001, null, 'watch', 0 ),
-(6, 'Tudor', '79830RB', 1000, null, 'watch', 0 );
+(1, 'Rolex', '1675', 6001, null, 'watch', 0 ),
+(2, 'Rolex', '16750', 4001, null, 'watch', 0 ),
+(3, 'Rolex', '16760', 4001, null, 'watch', 0 ),
+(4, 'Rolex', '126710', 6001, null, 'watch', 0 ),
+(5, 'Rolex', '6542', 9500, null, 'watch', 0 ),
+(6, 'Rolex', '16610', 3001, null, 'watch', 0 ),
+(7, 'Rolex', '16800', 2901, null, 'watch', 0 ),
+(8, 'Rolex', '168000', 3001, null, 'watch', 0 ),
+(9, 'Rolex', '116613', 5001, null, 'watch', 0 ), 
+(10, 'Rolex', '116610', 5001, null, 'watch', 0 ), 
+(11, 'Rolex', '216570', 4001, null, 'watch', 0 ), 
+(12, 'Rolex', '116500', 10001, null, 'watch', 0 ),
+(13, 'Omega', '311.30.42.30.01.005', 2001, null, 'watch', 0 ),
+(14, 'Tudor', '79830', 1200, null, 'watch', 0 );
+
